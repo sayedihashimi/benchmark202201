3fb28912907130d2197cc361cff176e93a0075f7
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-01-18 13:50:38 -0500

    adding newpage to angular project

--- AngularWeb/AngularWeb/ClientApp/src/app/app.module.ts
+++ AngularWeb/AngularWeb/ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import { NewpageComponent } from './newpage/newpage.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { TictactoeComponent } from './tictactoe/tictactoe.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    TictactoeComponent
+    NewpageComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +28,7 @@ import { TictactoeComponent } from './tictactoe/tictactoe.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'tictactoe', component: TictactoeComponent }
+      { path: 'newpage', component: NewpageComponent }
     ])
   ],
   providers: [],
