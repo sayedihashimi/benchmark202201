86cbdc7a32e153cd3023481a53bebfb3212004c4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-01-18 15:12:50 -0500

    configuring unit test project

--- GrpcApp/GrpcApp.Tests/UnitTest1.cs
+++ GrpcApp/GrpcApp.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Grpc.Net.Client;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcApp.Tests
 {
@@ -9,10 +12,25 @@ namespace GrpcApp.Tests
         {
         }
 
-        [Test]
-        public void Test1()
+[Test]
+public async Task Test1()
+{
+    var channel = GrpcChannel.ForAddress("https://localhost:7283");
+            
+    var client = new Greeter.GreeterClient(channel);
+    var response = await client.SayHelloAsync(
+        new HelloRequest
         {
-            Assert.Pass();
-        }
+            Name = "benchmark"
+        });
+
+    Console.WriteLine(response);
+    Assert.IsNotNull(response.Message);
+    Assert.True(string.Equals("Hello benchmark",response.Message));
+    //var response = await client.SayHelloAsync(
+    //    new HelloRequest { Name = "World" });
+
+    //Console.WriteLine(response.Message);
+}
     }
 }
